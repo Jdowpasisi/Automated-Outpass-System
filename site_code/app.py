2edc22e41d503a4de548986c4cdbcaf1859f445b
whoknows4 <vermaachintya7@gmail.com>
2024-11-09 10:24:09 +0530

    Add files via upload

--- site_code/app.py
+++ site_code/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, redirect, url_for, render_template, logging, request, flash
+from flask import Flask, redirect, url_for, render_template, logging, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, logout_user, login_required, login_user, current_user
+from flask_mail import Mail, Message  
 import sqlite3
+import flask_login
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = "29384c28rf98d784238h33h3h3dshbse13bbd32"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'email address same as below'
+app.config['MAIL_PASSWORD'] = '2 steps verification key not the gmail password'
+app.config['MAIL_DEFAULT_SENDER'] = 'email address'
+mail = Mail(app)
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 
-class user(db.Model):
+class user(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(40))
     email = db.Column(db.String(45))
@@ -15,20 +32,22 @@ class user(db.Model):
 
 class outpass(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(40), unique = True)
-    mobile = db.Column(db.String(10), unique = True)
-    hostel_name = db.Column(db.String(30), unique = True)
-    hostel_number = db.Column(db.String(15), unique = True)
-    date = db.Column(db.String(30), unique = True)
-    leaving_date = db.Column(db.String(30), unique = True)
-    place = db.Column(db.String(50), unique = True)
-    leaving_time = db.Column(db.String(15), unique = True)
-    return_time = db.Column(db.String(15), unique = True)
-    parent_mobile = db.Column(db.String(10), unique = True)
-    program = db.Column(db.String(20), unique = True)
-    reason = db.Column(db.String(1000), unique = True)
+    name = db.Column(db.String(40))
+    mobile = db.Column(db.String(10))
+    email_add = db.Column(db.String(50), nullable = False)
+    hostel_name = db.Column(db.String(30))
+    hostel_number = db.Column(db.String(15))
+    date = db.Column(db.String(30))
+    leaving_date = db.Column(db.String(30))
+    place = db.Column(db.String(50))
+    leaving_time = db.Column(db.String(15))
+    return_time = db.Column(db.String(15))
+    parent_mobile = db.Column(db.String(10))
+    program = db.Column(db.String(20))
+    reason = db.Column(db.String(1000))
+    status = db.Column(db.String(20), default = 'pending')
 
-class admnuser(db.Model):
+class admnuser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     admin_name = db.Column(db.String(30), unique = True, nullable = False)
     admin_email = db.Column(db.String(50), unique = True, nullable = False)
@@ -46,7 +65,10 @@ def login():
 
         login = user.query.filter_by(username = usrname, password = pssword).first()
         if login is not None:
+            flash("logged-in sucsess", category = "success")
+            login_user(login)
             return redirect(url_for("home"))
+    
     return render_template("login.html")
 
 @app.route("/signup", methods = ["GET", "POST"])
@@ -88,9 +110,10 @@ def adminlogin():
         emailaddrs = request.form["emailaddrs"]
         psskey = request.form["psskey"]
 
-        login = admnuser.query.filter_by(admin_email = emailaddrs, passkey = psskey).first()
+        adminlogin = admnuser.query.filter_by(admin_email = emailaddrs, passkey = psskey).first()
         if adminlogin is not None:
             return redirect(url_for("admin"))
+        flash("something invalid", category="error")
     return render_template("adminlogin.html")
 
 @app.route("/form", methods = ["GET", "POST"])
@@ -98,6 +121,7 @@ def form():
     if request.method == "POST":
         uname = request.form["uname"]
         num = request.form["num"]
+        mail = request.form["mail"]
         hname = request.form["hname"]
         hnum = request.form["hnum"]
         tarikh = request.form["tarikh"]
@@ -109,16 +133,53 @@ def form():
         prgm = request.form["prgm"]
         rson = request.form["rson"]
 
-        form = outpass(name = uname, mobile = num, hostel_name = hname, hostel_number = hnum, date = tarikh, leaving_date = ldate, place = vplace, leaving_time = ltime, return_time = ctime, parent_mobile = pnum, program = prgm, reason = rson)
+        form = outpass(name = uname, mobile = num, email_add = mail, hostel_name = hname, hostel_number = hnum, date = tarikh, leaving_date = ldate, place = vplace, leaving_time = ltime, return_time = ctime, parent_mobile = pnum, program = prgm, reason = rson)
         db.session.add(form)
         db.session.commit()
 
         return redirect(url_for("about"))
     return render_template("form.html")
 
+@app.route('/approve/<int:id>', methods = ['POST'])
+def approved_mail(id):
+    outpass_ord = outpass.query.get(id)
+    if outpass_ord:
+        outpass_ord.status = 'approved'
+        db.session.commit()
+
+        send_mail(outpass_ord.email_add, outpass_ord.name)
+
+    return redirect(url_for("admin"))
+
+@app.route('/reject/<int:id>', methods = ['POST'])
+def rejected_mail(id):
+    outpass_ord = outpass.query.get(id)
+    if outpass_ord:
+        outpass_ord.status = 'rejected'
+        db.session.commit()
+
+    return redirect(url_for("admin"))
+
+def send_mail(student_email, student_name):
+    subject = "Your outpass has been approved"
+    body = f"Hello {student_name}, \n\n Your application has been approved, Thank you."
+
+    msg = Message(subject, recipients = [student_email])
+    msg.body = body
+
+    try:
+        mail.send(msg)
+        print(f"mail sent to {student_email}")
+    except Exception as e:
+        print(f"fail to send email {str(e)}")         
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user.query.get(int(user_id))
 
 with app.app_context():
 
